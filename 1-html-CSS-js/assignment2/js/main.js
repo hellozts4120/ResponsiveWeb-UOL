2b4dfa9435452aa405b5b23933ecae8271ff7f73
hellozts4120 <14307110190@fudan.edu.cn>
2016-04-20 17:02:12 +0800

    back up for class1

---
+++ 1-html-CSS-js/assignment2/js/main.js
@@ -0,0 +1,12 @@
+$('#button1').click(function() {
+    $('#word1').addClass("show");
+});
+
+$('#button2').click(function() {
+    console.log('test2');
+    $('#word2').addClass("show");
+});
+
+$('#button3').click(function() {
+    $('#word3').addClass("show");
+});
