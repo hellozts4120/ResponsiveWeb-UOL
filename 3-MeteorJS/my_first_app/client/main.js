0770b7f9e89dd06dac552776986e68596718cc4e
hellozts4120 <14307110190@fudan.edu.cn>
2016-05-26 20:49:44 +0800

    meteorJs class week1

---
+++ 3-MeteorJS/my_first_app/client/main.js
@@ -0,0 +1,8 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+if (Meteor.isClient) {
+    Template.hello.helpers({time:new Date()});
+}
